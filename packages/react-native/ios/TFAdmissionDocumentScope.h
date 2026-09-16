30dfb1376a34034e1166d666e243fd5d78dc0e64
prjless <nachoab@live.com>
2026-09-16 19:54:08 +0200

    feat(ios): declare the private prepared-document admission scope [skip ci]

---
+++ packages/react-native/ios/TFAdmissionDocumentScope.h
@@ -0,0 +1,7 @@
+#import "TFNativeDocumentRegistry.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT TFNativeDocumentScope *TFCreateAdmissionDocumentScope(TFAdmissionEngine *engine, NSString *context);
+
+NS_ASSUME_NONNULL_END
